42887df0aa2b7126253a25855ee1b0e72f03fe33
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 01:46:00 -0800

    Create SellPage.js
    A first commit of the blockchain dispensary buy/sell SELL PAGE. The sell page will have top page listings like all other pages in the header and it the body 1-3 images will appear in the body advertising items for sell on the blockchain! There is a list item for sell link to another page option to use social profile to list item for sell and submit the item to the first sector of approval of item human verified hash earning a reward from a confirmation on the block dispensary website! there is a click buy item button just incase you see an item to buy while going to list an item for sell! im javascript skills lack any help or suggestions would be appreciated i am just using the late night early morning to write the basic HTML route to outline the project!

---
+++ SellPage.js
@@ -0,0 +1,31 @@
+type-script=javescript
+<html>
+    <header>
+        <h1 style = align-center> Block Dispensary Sell Page </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <body>
+            var ItemDisplay = if (display.item = true) {
+                Response.load(item.display(5) = loadNew)
+            }
+               const Displayed = function(<loadNew className="Item">imageType</loadNew>(5seconds)){
+                display.getItem = ItemDisplay(loadNew) }
+                loadImage == function(Displayed)
+               {}
+               <h1>Items for sale</h1>
+               await functionName() == Displayed; "displayed itemMaxSize"
+               <h1 href="BuyItem.js">Buy Item</h1>
+               <h1 href="BrowseItems.js">Browse Other Items</h1>
+               <h1 href="ChooseCatergory.js">Choose another catergory</h1>
+             </body> 
+        </header> 
+ </html>
