4ffc69ad15f7a871ca8d97f48f45e00e408bd3c1
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 12:22:04 -0800

    Create approval.js
    Approval page is to be approved for a postion as a human hasher on the send item to sell block on the Block Dispensary Blockchain simple registration detials with submit button guideline and policies explained in short detail. example explanation of what to expect of the procedure of human hash function on the Block Dispensary Blockchain approve item to Sell blockchain the first of four sector blockchains on the Block Dispensary Blockchain Network. The top of the approve page being the same as all the other pages with link redirects to the websites other pages!

---
+++ approval.js
@@ -0,0 +1,52 @@
+<html>
+    <header>
+        <h1 style = align-center> Block Dispensary Sell Page </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <h1 style "align-text=center; text-size=100px">GET APPROVED</h1>
+        <h1>Dont have an account Profile?<h1>
+        <h1 href="SignUp.js>Sign up for account Now</h1>
+        <div1>
+        <h1>Start a Human Hash verification application</h1>
+        <P>When signing up for human hash verification follow the steps to proceed to becoming a professional human hash for reward from Block Dispensary Blockchain Transactions Get paid in BTC or opt in to auto exchange to a different Crypto currency of your choice</P>
+        <h1>Registration Portfolio</h1>
+        <h>Enter your details {
+            NAME:_____________,
+            COUNTRY:_____________,
+            Date Of Birth:_______________
+            Current Job:________________
+            Reason for Applying:_________________
+            Education:_______________
+            Hobbies & Intrests:________________
+            Other Details_______________',
+            <h1 button type="submit">SUBMIT</button>
+            </div1>
+            <div2>
+            <p>when finished submitting information please read guidelines and policies of Block Dispensary Human Hash Verification Conditions</p>
+            <h1>Guidelines and Policies</h1>
+            <p>The guidelines and policies are to provide equal chance on obtaining a time period daily weekly or monthly to be in position and in que to recieve items to human hash. With human hash cycles depending on how many items are submitted to the blockchain there is not guarantee that your selected time will provide Items to verify by human hash on the Block Dispensary Blockchain. Human Hash verification spots will depend solely on low or high demand in need for human hash on the blockchain transactions. Equal consideration to all people who opt in to human hash on notification of item with no set time period will be first come first serve. So if you are the first one to see the new notification for the need of human hash and accept the duty you get the spot of that items hash in no way is Block Dispensary liable for any time not provided with a item to hash or result in no items to hash for long periods of times. times scheduled to hash items by pre registered profiles will have first entitlement to items sent to the blockchain for hashing max length in pre registered time to hash is 3 hours after the 3 hours a 3 hour wait period is applied until next available pre scheduled human hash period however after the first pre registered time period you can still human hash items on blockchain only by items that give notification and are not being hashed by pre registed human hash profiles! </p>
+            <h1>Procedure of Human hash function</h1>
+            <h1>Step one</h1>
+            <p>The procedure to human hash verification is quiet simple its simply recieve the item details such ash photo name details and all other content to due with item listing and state whether the item description is true in sellers description statement a quick read over the item in the blockchain and look at photos as with genuine honest human approval a approve item for listing on sell blockchain will be clicked and approved providing a true honest human hash function in the Block Dispensary first Blockchain "approval blockchain"</p>
+            <h1>Step 2</h1>
+            <p>After client providing human hash verification approval will need to follow human verification a simple task providing a small puzzle or quiz to verify human statue</p>
+            <h1>Step 3</h1>
+            <p>some items will be through the full Block Dispensary Blockchain faster than others However there may be a delay in seeing Human Hash verification reward due to delivery times and buyer approval some items like digital downloads like music sales or digital photo sales with quick immeadiate delivery will post reward faster than a physical item sent by courier delivery</p>
+            <h1>Finished Now Be Rewarded in BTC or auto exchange to a crypto on Block Dispensaries coin market</h1>
+            <div2>
+                <div3>
+                <p>The Block Dispensary is always maintaining a security policy and enjoying the social engagement on our blockchain social profiles we do not sell data to any government or corporation</p>
+                <p>Block Dispensary is evolving the digital currency demand giving more options to using digital currency for purchase of items</p>
+            <h1>BLOCK DISPENSARY</h1>
+            </div3>
+            </header>
+            </html>
