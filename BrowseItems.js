9f9b3419c237bf17b06bf3c6628f17119e709f92
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 04:46:26 -0800

    Create BrowseItems.js
    the Browse Items for sale on the Block dispensary Blockchain page with 3 images by 2 columns  could be more columns and rows changing items every 5 - 10 seconds revolving on a scale displaying a new image out of the 6 displayed every 5 seconds giving an option to click on the photo and be redirected to the item purchase page! the revolving to the right as far until out of page view and then on the left displaying new image! a  link to list item on the browse page to be redirected to list to sell page! A brief mention of social networking on the website and that us as block dispensary do not sell personal data to governments or corporations mention of evolving digital currency demand opening up more options to purchase items with online digital currencies!

---
+++ BrowseItems.js
@@ -0,0 +1,29 @@
+<html>
+    <header>
+        <h1 style = align-center> Block Dispensary Sell Page </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <p>Thanks for visiting the Browse items for sale page on this page you will see confirmed items from the first hash sector all items if shown are currently for sale for price listed! You can switch between catergories of items for sell on Block Dispensary Blockchain Buy and sell </p>
+
+        <h1 style = align-center>Browse Page Showing Items for sale on the Block Dispensary Blockchain</h1>
+        const A = display.Image(if true Image.display);
+        function display = Image.data(5secs = display.time) 
+        var src-image = live.require(if 5secs return(new.value = image.display))
+        src-image call.data void if display.Image{<= 5secs}
+        onclick.Item(photo.jpg) == redirect.url.ItemPage
+        <h1 src-image = "photo.jpg"> Item one for sale </h1> <h1 src-image = "photo.jpg"> Item two for sale</h1> <h1 src-image = "photo.jpg"> Item three for sale</h1>
+        <h1 href="listitem.js> List your item on the Block dispensary Blockchain </h1>
+        <h1 src-image = "photo.jpg"> Item four for sale </h1> <h1 src-image = "photo.jpg"> Item five for sale </h1> <h1 src-image = "photo.jpg">Item six for sale</h1>
+        <p>The Block Dispensary is always maintaining a security policy and enjoying the social engagement on our blockchain social profiles we do not sell data to any government or corporation</p>
+        <p>Block Dispensary is evolving the digital currency demand giving more options to using digital currency for purchase of items</p>
+        <body></body>
+        </header>
