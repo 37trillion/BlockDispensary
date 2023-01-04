a2e1a57e2e50d566171ea5be8a494d40ab98aab3
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 15:32:35 -0800

    Create Blockchain.js
    an example of the blockchain page for block dispensary with download option to download file for the block dispensary blockchain explanation of the block dispensary blockchain and the four sector blocks and how they are hashed to complete the main block confirmation! original top page links and BLOCK DISPENSARY at the bottom with a logo for block dispensary! where it states public chain data for the block dispensary the public chain for the blockchain could be displayed on this page or redircted to a different javascript page or redirected to a different website for block dispensary blockchain explorer!

---
+++ Blockchain.js
@@ -0,0 +1,49 @@
+<html>
+    <header>
+        <h1 style = align-center> Block Dispensary Sell Page </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <h1 style "align-text=center; text-size=100px">GET APPROVED</h1>
+        <h1>Do not have an account Profile?<h1>
+        <h1 href="SignUp.js">Sign up for account Now</h1>
+        <div1>
+        <h1 "style = align-center" BLOCKCHAIN FOR BLOCK DISPENSARY </h1>
+        <h1> Blockchain for Block Dispensary Public Chain Data </h1>
+        //data displayed being able to click from page 1 to current block on block dispensary block data explorer or link to another java file or website
+        <p> Block Dispensary supplies a public chain of data keeping personal identity personal details anonymous, opt in to have buyer and seller identities listed as user profile name or opt in to be placed in block as unknown. since block genesis all public data is stored with Block number, Timestamp, Item description, Value of total transaction including price of item, shipping fees, and transaction fees. The Block Dispensary Blocks will also include Time item was sent and time item was delivered under confirmation providing value information data on the items that are sold what time they are sold at what time they are delivered and what time the final block sector of a four sector block function is completed </p>
+        <h1> The Four Block Sectors of the Block dispensary Blockchain </h1>
+        <p> the four block sectors of a complete block on the block dispensary blockchain are number one Human hash verification to approve truth of item details to release item into the SELL Block while in the sell block the item remains in the Sell Block until a purchase made through the BUY page second sector of the Block Dispensary Complete Block is that being second sector of cpu hash on Buy or purchased Block with CPU hash applying Hash to secure transaction value until Buy Block transaction value is transfered from BUY account to Sell Account sometimes delayed by a short or long period of time depending on delivery times the third sector of the four sector complete block is the third sector delivery block sector Delivery sector in the block can be confirmed instantly with digital sales or confirmed when API tracking order delivery status on block 3 sector information is confirmed by cpu the delivery service or courier has an option to opt into recieving some of the reward from transaction fees for human verification by delivery personnel human verification on the delivery block can also be confirmed by recipient or buyer when recieved at delivery location and approved as in good condition and recieved status miners will be reward 50 percent of the transaction fees with 30 percent of the fees going to charities around teh world and 20 percent going back into the Block Dispensary Business development! </p>
+        <h1> Sector one: Approve block sector Human Hash Verification approve Item details as true 
+             Sector two: Buy or purchased block sector CPU Hash for security of buy transaction to get funds to seller and secure transaction fees
+             Sector three: Delivery block sector a human and cpu by api delivery tracking confirmation delivery personnel opt in/out to human hash verification buyer human hash verification confirm item condition 
+             Sector four: Block CPU hash final complete block confirmation publish block details to block dispensary blockchain as complete block </h1>                 
+        </h1>
+        <div2>
+        <p> Download the Block Dispensary Blockchain to start hashing CPU power to provide the Block Dispensary security on transactions to guarantee seller recieves payment and transaction fees are secured to the value ratio to each element of those who contributed to the Hash of each item transaction </p>
+        <h1> Block Dispensary Blockchain </h1>
+            <h1> button wx.downloadFile({
+                //example url to download blockchain block dispensary exe file and contents
+                url: 'www.dropbox.com/BlockDispensary.zip',
+                type: xip,
+                success: function(res) {
+                    // do something
+                }
+            })
+            </h1>
+              </div2>
+              <div3>
+             <p>The Block Dispensary is always maintaining a security policy and enjoying the social engagement on our blockchain social profiles we do not sell data to any government or corporation</p>   
+                <p>Block Dispensary is evolving the digital currency demand giving more options to using digital currency for purchase of items</p>
+            <h1>BLOCK DISPENSARY</h1>
+            </div3>
+            </header>
+            </html>
