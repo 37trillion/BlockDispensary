ff5209d88be2c2b83f6fbcb48a8e08e2cb974e19
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 11:30:39 -0800

    Create ListItem.js
    Here is the list item page consisting of credentials of listing a physical item or digital picture to the Block Dispensary Blockchain list item sell price in bitcoin but opt in to sell item for other eligible crypto currencies! Upload photo write description name item another upload music or podcast to blockchain option below main item upload! top of page with all the links to other pages as in all the other pages! Item is submitted and then sent to human hash verification on sector one of the Block Dispensary blockchain!

---
+++ ListItem.js
@@ -0,0 +1,54 @@
+<html>
+    <header>
+        <h1 style = align-center> Block Dispensary Sell Page </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <div1>
+        <h2> This is the page to engage on the Block Dispensary Blockchain and sell your item for crypto currency for a small economical fee! </h2>
+        <h1 style = "text-size=50px,align-center">Upload item Image</h1>
+        <h1 style = "upload-photo" img src="./image-upload" alt="./image-upload"uploadButton-type="ghost" icon="ios-cloud-upload-outline">Upload Item Image</Button></upload>
+        <h1>Set Price for item in BTC ________</h1>
+        <h1>Item description provide details of item with details on quality of item new or used if uploading a music or podcast piece please click link "upload music or podcast" below.</h1>
+        <h1>Name of Item:
+            *
+            *
+        <h1 dialogBox style = "dialog-box-align=center">Write description:
+            *
+            *
+            *
+            *
+        </h1>
+        <h1 style = "interactive-text">List Block item tags:
+            *
+            *
+            *
+            *
+        </h1>
+        <h1>Submit Item to Block Dispensary Blockchain to be approved by Human Hash confirmation</h1>
+        <h1 button type="submit">SUBMIT to BLOCKCHAIN</button></h1>
+        <h1>Submit Music or Podcast</h1>
+        <h1 button type="submit" upload-file> 
+        wx.uploadFile({
+            url: '',
+            filePath: filePath,
+            name: 'file',
+            file-upload:
+        })
+        upload</upload-file></button></h1>
+        </div1>
+        <div2>
+        <p>The Block Dispensary is always maintaining a security policy and enjoying the social engagement on our blockchain social profiles we do not sell data to any government or corporation</p>
+        <p>Block Dispensary is evolving the digital currency demand giving more options to using digital currency for purchase of items</p>
+        <h1 "align-text = center">BLOCK DISPENSARY</h1>
+        </div2>
+         </header>
+        </html>
