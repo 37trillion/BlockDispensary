660a626733131cddf244d3d5e9a8760cb1334f00
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 13:36:29 -0800

    Create SignUp.js
    Sign up page is the Block Dispensary website profile sign up page at the top of the page the normal links to other pages, A step by step process to filling in profile details uploading photo and submitting profile application to Block Dispensary Website to gain access to sell and buy plus human hash on the website! once registered with a profile your profile page will become accessible where comments on items for sell can be posted, "Post a new Block Talk" will be profile que on posting a profile update! Homies option to Homie another profile like a follow or friend request will be available to build a block dispensary social network! Hash Rewards opt in opt out to be displayed on profile page. the data from public accounts of "Post a new Block Talk" will result in the "Block Talk Chain Cycle" providing instant access to viewing public posts from POST A NEW BLOCK TALK to result in the BLOCK TALK CHAIN CYCLE updated cycle of posts previous sells and previous buys option opt in opt out for profile display! Change profile setting from profile setting tab button! pick a unique account block handle instead of your given name!

---
+++ SignUp.js
@@ -0,0 +1,50 @@
+<html>
+    <header>
+        <h1 style = align-center> Block Dispensary Sell Page </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <h1 style "align-text=center; text-size=100px">GET APPROVED</h1>
+        <h1>Dont have an account Profile?<h1>
+        <h1 href="SignUp.js">Sign up for account Now</h1>
+        <p>Block Dispensary is Building a stronger acceptance for Blockchain, Bitcoin, and Crypto Currency Markets Giving more reasons to buy and hold Bitcoin or crypto currency with peoples support and peoples time spent to list items on the Block Dispensary Blockchain it will build a stronger blockchain community and greater market for buying with online digital currencies i believe everything should be able to be bought with online currencies globally and in any market or price</p>
+        <h1 style = "align=center">CREATE BLOCK DISPENSARY BUY & SELL PROFILE</h1>
+        <p>create a profile on the Block Dispensary Website by Following Steps below</p>
+        <div>
+            <h1>STEP ONE</h1>
+            <h1>UPLOAD PROFILE IMAGE</h1>
+        <Upload action=".JPG">
+            <Button type="ghost" icon="ios-cloud-upload-outline">Upload Image </Button></Upload>
+        <h1 function(uploadImage = {Image.require} + (async = FileUpload.img))> Choose Image to upload </h1>
+        </div>
+        <div1>
+        <h1>STEP TWO</h1>
+        <h1> FILL IN PROFILE DETAILS </h1>
+        <h1>Enter Profile Details:
+            *NAME:_____________________
+            *DOB:_____________________
+            *COUNTRY:____________________
+            *PROVINCE:______________________
+            *CITY:______________________
+            *PHONE:_____________________
+            *EMAIL:_______________________
+            *OTHER:_______________________
+            
+        </h1>
+        <h1>STEP THREE</h1>
+        <h1 button type="Submit">SUBMIT</button></h1>
+        </div1>
+        <div2>
+        <p>A Block Dispensary Profile will give you access to easily selling or buying on the Block Dispensary Blockchain Network, after initial profile sign-up register to become a human hash verifier in a quick simple process</p>
+        <h1>BLOCK DISPENSARY</h1>
+        </div2>
+        </header>
+        </html>
