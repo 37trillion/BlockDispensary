0fbeaca9f5e69fdc06ba3e945e5b29d5927e1506
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 19:02:45 -0800

    Create CurrentBuy.js
    Current Buy page displays a slide show from left to right items on buy sector block on the Block Dispensary Blockchain with a 7 second delay on each item shifting from left start to right rotation slide show of items on buy sector block waiting to be hashed by cpu mining hash with homie profiles on the Block Dispensary Blockchain Buy and Sell on the Example item on buy sector item number is listed description as in example cd player is include in the description was human hashed verified in the sell sector block previously timestamp showing date of item arriving on the Buy sector block the homie-tag if opt in to display homie tag if NULL just HOMIE will be displayed the image then is displayed at the bottom of example item on buy sector slideshow of current items on buy sector waiting for pc hash to confirm buyers purchase value is transfered to sellers wallet witch could be delayed in physical goods due to delivery time length varies digital goods would almost transfer assets instantly! cpu hash confirms transaction and transfers value to sellers wallet and transaction fees are distributed in witch could have a reward deposit delay due to delivery sector delivery times!

---
+++ CurrentBuy.js
@@ -0,0 +1,49 @@
+<html>
+    <header>
+        <h1 style = align-center> Block Dispensary Sell Page </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <h1 style "align-text=center; text-size=100px">GET APPROVED</h1>
+        <h1 href="SignUp.js">Sign up for account Now</h1>
+        **//
+            DIV 1 displays simple data
+        **//
+        <div1>
+            <h1>Current buy blockchain sector</h1>
+            <p>This page displays current items that are purchased and on the buy sector of the block dispensary block sectors Items displayed in this current buy page are already purchased from the sell sector and are currently being approved by cpu hash verification of items transaction values securing the route of currency get to the sellers wallet from the buyers wallet with transaction fees being secured for rewards for miners charities and Block dispensary operations information provided of each physical and digital asset as a item description is provided, timestamp stating time of of item entering the buy sector a publish of profiles homie tag if opt in attached to the items block cpu hash provides hash on the items block until securely secured transaction confirmation. a time frame long or short may be in effect for miner rewards to be published to homie profile miner rewards due to delivery sector block time period differences short or long your guaranteed to get you cpu hash reward for cpu hash on buy block sector.</p>
+            <h1>Buy Sector Blockchain</h1>
+            </div1>
+            **//
+             DIV 2 starts slide show of current block sector buy items from left with each item in time moves to the right of the page on a 7 second display shift from left to right and loops over
+            **//
+            <div2>
+            <h1>
+                EXAMPLE ITEM ON BUY SECTOR
+                Item: 00000000000000000001
+                description: CD player for playing CDs in a retro sleek design brand new@
+                timestamp: 01/06/2023
+                homie-tag: Homie250
+                Image:<image-File>copy path of img.jpg</image-File>
+            </h1>
+            </div2>
+            **//
+                DIV 3 displays simple data of same structure to each ending page segment
+            **//
+            <div3>
+            <p>The Buy sector is cpu hashed for secure transfer of buyers paid sum of value to transfer to sellers wallet with Block Dispensary transaction fee to support miners charities and Block Dispensary Business development</p>
+            <h1>Want to Join on Cpu Hash Duties and earn Reward for CPU hash to secure transactions?</h1>
+            <h1 link rel="" type="" href="CLICK HERE TO JOIN OTHER HOMIES ON CPU HASH DUTIES"></h1>
+            <p>The Block Dispensary is always maintaining a security policy and enjoying the social engagement on our blockchain social profiles we do not sell data to any government or corporation</p>
+            <p>Block Dispensary is evolving the digital currency demand giving more options to using digital currency for purchase of items</p>
+            <h1 "align-text = center">BLOCK DISPENSARY</h1>
+            </div3>
+            </html>
