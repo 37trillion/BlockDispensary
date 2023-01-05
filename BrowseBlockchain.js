dad8ce9a27a781c0d89a19400705bddb59f396f0
37trillion <96641975+37trillion@users.noreply.github.com>
2023-01-04 23:26:49 -0800

    Create BrowseBlockchain.js
    An example of browse blockchain page with normal top of page with links to Block Dispensary Blockchain Browser allowing Public viewing of every item listed on the blockchain on the Block Dispensary Blockhain! as seen in script block is labelled with with information on block details at this moment i believe i am goin to add homie-tag social profile names for human hasher and Homie-tag for cpu hasher the homie-tags shown are for buyer and seller! no personal identity will be exposed of on Block information except for social profile homie name! a interactive number of block list at top of display of block as seen above a click body of current displayed block swipe left to go down block numbers swipe right to go up block! block dispensary block browswer example!

---
+++ BrowseBlockchain.js
@@ -0,0 +1,40 @@
+<html>
+   <h1 style = align-center> Block Dispensary </h1>
+        <p>Block Dispensary Sell page providing a social profile to sell physical/digital assets </p>
+        <h1 href="listitem.js">LIST ITEM </h1>
+        <h1 href="approval.js">Get Approval for Human verification Hash</h1>
+        <h1 href="Browse.js">Browse Items for sale</h1>
+        <h1 href="Blockchain.js">Go to Blockchain Dispensary</h1>
+        <h1 href="CurrentSell.js">Check out the current Blockchain Dispensary Sell Page</h1>
+        <h1 href="CurrentBuy.js">Locate Buy Blockchain Dispensary Current transactions</h1>
+        <h1 href="DeliveryPublic.js">See current Blockchain Dispensary Public Delivery Blockchain</h1>
+        <h1 href="BrowseBlockchain.js">Browse all four block sectors on the public Blockchain Dispensary Public Blockchain</h1>
+        <h1 href="JoinCpu.js">Join the Miner Block Reward CPU Blockchain Dispensary</h1>
+        <h1 href="SignUp.js">Sign Up for Block Dispensary Buy & Sell Profile start Selling/Buying/Hashing</h1>
+        <h1 style "align-text=center; text-size=100px">GET APPROVED</h1>
+        <h1>Do not have an account Profile?<h1>
+        <h1 href="SignUp.js">Sign up for account Now</h1>
+        
+<h1 style = "align-text=center" "text-size100px">Blockchain Browser</h1>
+<h1>Explore the Block Dispensary Browser</h1>
+<p style = "align-text=center">Block Dispensary declares all blocks on a Blockchain Browser visible to anyone who comes across this Blockchain Browser personal correct Identity never will be posted to Block Dispensary Browser accept for Homie self assigned name from the profiles of BUY and SELL homies</p>
+<h1>Block Dispensary Browser</h1>
+**//
+    Example of Block Dispensary Browser Design Select Block with block by number interactive click button or click block body display and slide to the left or right left going down the block right going up the block
+**//
+<h2 click = button.interactive> Block:0-1-2-3-4-5-6-7-8-9..20 </h2>
+<h2 "align-text=center"> Block Gensis </h2>
+<h2>            Item: 000000000000000000001</h2>
+<h2>            description: CD player for playing CDs in a retro sleek design brand new
+                timestamp: 01/06/2023
+                homie-tag: Homie250
+                homie-tag: Homie604
+                Image:<image-File>copy path of img.jpg</image-File>
+                </h2>
+
+                <div3>
+                <p>The Block Dispensary is always maintaining a security policy and enjoying the social engagement on our blockchain social profiles we do not sell data to any government or corporation</p>
+                <p>Block Dispensary is evolving the digital currency demand giving more options to using digital currency for purchase of items</p>
+            <h1>BLOCK DISPENSARY</h1>
+            </div3>
+            </html>
